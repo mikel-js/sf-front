f4b5e1590d67d241b07a4e8898d7b3eed48e64f2
mikel-js <michaelcastro.js@gmail.com>
2023-10-29 12:59:38 +0200

    Add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import { useState } from 'react';
 
 function App() {
-  return <>test</>;
-}
+  const [userName, setUserName] = useState<string | undefined>(undefined);
 
-export default App;
+  const router = createBrowserRouter([
+    {
+      element: (
+        <>
+          <NavBar userName={userName} />
+          <Outlet />
+        </>
+      ),
+      children: [
+        {
+          path: '/',
+          element: <div>Hello world!</div>,
+        },
+        {
+          path: '/login',
+          element: <div>Login page</div>,
+        },
+        {
+          path: '/profile',
+          element: <div>Profile page</div>,
+        },
+        {
+          path: '/createSpace',
+          element: <div>Create space page</div>,
+        },
+        {
+          path: '/spaces',
+          element: <div>Spaces page </div>,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <div className='wrapper'>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
